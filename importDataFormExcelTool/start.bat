ae9f4876325e26aa2c0bece694c0f81ab3d6ff79
sanze <hunter0122@163.com>
2016-06-28 22:07:59 +0800

    .bat及config文件修改，ktr流程文件修改

--- importDataFormExcelTool/start.bat
+++ importDataFormExcelTool/start.bat
@@ -7,4 +7,4 @@ set JAVA_HOME=..\jdk
 set PATH=%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
 set CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar
 ::调用job任务
-kitchen /file D:\CBB_PF\importDataFormExcelTool\job.kjb
+kitchen /file ../job.kjb
